da54b5da5bbef65bb4f6962de7a9f82f878dea35
Mu-An Chiou <me@muanchiou.com>
2022-08-07 16:51:34 +0800

    Add fragment link

--- dist/index.js
+++ dist/index.js
@@ -19,7 +19,7 @@ function css(duration) {
     background-color: rgba(0, 0, 0, 0.9);
   }
 
-  #close,
+  #controls #close,
   #play,
   #pause {
     display: none;
@@ -122,9 +122,14 @@ function css(duration) {
     position: absolute;
     margin: 20px 10px 10px;
     display: flex;
-    gap: 1em;
+    gap: 10px;
     align-items: center;
     z-index: 1;
+  }
+
+  #controls button,
+  #controls a {
+    display: inline-flex;
   }
 
   #time {
@@ -134,14 +139,9 @@ function css(duration) {
     text-shadow: 0 0 5px #000;
   }
 
-  #play-pause svg,
-  #close svg {
+  #controls svg {
     width: auto;
     height: 3.5vh;
-  }
-
-  #play-pause,
-  #close {
     line-height: 0;
   }
 
@@ -285,8 +285,8 @@ function css(duration) {
       --magic-w: 100vw;
     }
 
-    #close {
-      display: block;
+    #controls #close {
+      display: inline-flex;
     }
   }
 `;
@@ -312,9 +312,14 @@ class StoryViewElement extends HTMLElement {
         <div id="bars"></div>
         <div id="controls">
           <span id="time"></span>
+          <a href="" id="link">
+            <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path fill-rule="evenodd" clip-rule="evenodd" d="M9.41489 9.17763C9.80542 8.78711 9.80542 8.15395 9.41489 7.76342V7.76342C9.02437 7.3729 8.3912 7.3729 8.00068 7.76342L6.92857 8.83553C5.757 10.0071 5.757 11.9066 6.92857 13.0782C8.10014 14.2497 9.99964 14.2497 11.1712 13.0782V13.0782C11.3254 12.924 11.3254 12.6739 11.1712 12.5197L10.3154 11.664C10.1612 11.5098 9.9112 11.5098 9.757 11.664V11.664C9.36647 12.0545 8.73331 12.0545 8.34278 11.664C7.95226 11.2734 7.95226 10.6403 8.34278 10.2497L9.41489 9.17763ZM11.5918 9.82911C11.2013 10.2196 11.2013 10.8528 11.5918 11.2433V11.2433C11.9824 11.6338 12.6155 11.6338 13.0061 11.2433L13.9996 10.2497C15.1712 9.07817 15.1712 7.17868 13.9996 6.00711C12.8281 4.83553 10.9286 4.83553 9.757 6.00711V6.00711C9.64616 6.11794 9.64616 6.29763 9.757 6.40847L10.7698 7.42132C10.8807 7.53215 11.0604 7.53215 11.1712 7.42132V7.42132C11.5617 7.03079 12.1949 7.03079 12.5854 7.42132C12.9759 7.81184 12.9759 8.44501 12.5854 8.83553L11.5918 9.82911Z" fill="white"/>
+            </svg>
+          </a>
           <button id="play-pause" type="button" aria-label="Play/Pause" aria-pressed="true">
             <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg" id="play">
-              <path d="M6 13.0568V6.94319C6 6.12982 6.91937 5.65669 7.58124 6.12946L11.8608 9.18627C12.4191 9.58509 12.4191 10.4149 11.8608 10.8137L7.58124 13.8705C6.91937 14.3433 6 13.8702 6 13.0568Z" fill="white"/>
+              <path d="M7 13.0568V6.94319C7 6.12982 7.91937 5.65669 8.58124 6.12946L12.8608 9.18627C13.4191 9.58509 13.4191 10.4149 12.8608 10.8137L8.58124 13.8705C7.91937 14.3433 7 13.8702 7 13.0568Z" fill="white"/>
             </svg>
             <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg" id="pause">
               <rect x="7" y="6" width="2" height="8" rx="1" fill="white"/>
@@ -323,8 +328,8 @@ class StoryViewElement extends HTMLElement {
           </button>
           <button id="close" type="button" aria-label="Close">
             <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <rect x="6" y="7.41422" width="2" height="10.5255" rx="1" transform="rotate(-45 6 7.41422)" fill="white"/>
-              <rect x="7.41422" y="14.8568" width="2" height="10.5255" rx="1" transform="rotate(-135 7.41422 14.8568)" fill="white"/>
+              <rect x="6" y="7.35723" width="1.91942" height="10.1014" rx="0.95971" transform="rotate(-45 6 7.35723)" fill="white"/>
+              <rect x="7.35724" y="14.5" width="1.91942" height="10.1014" rx="0.95971" transform="rotate(-135 7.35724 14.5)" fill="white"/>
             </svg>
           </button>
         </div>
@@ -337,9 +342,10 @@ class StoryViewElement extends HTMLElement {
       </dialog>
     `;
         this.dialog = this.root.querySelector('dialog');
-        this.button = this.root.querySelector('#trigger');
-        this.close = this.root.querySelector('#close');
+        this.button = this.root.querySelector('button#trigger');
+        this.close = this.root.querySelector('button#close');
         this.meta = this.root.querySelector('#metadata');
+        this.link = this.root.querySelector('a#link');
         this.time = this.root.querySelector('#time');
         this.goToBinding = this.goTo.bind(this, 1);
     }
@@ -401,7 +407,9 @@ class StoryViewElement extends HTMLElement {
         this.dialog.addEventListener('close', () => {
             if (this.timer)
                 clearTimeout(this.timer);
-            this.currentIndex = 0;
+            this.currentIndex = -1;
+            if (this.itemByHash())
+                window.location.hash = '';
         });
         playPause.addEventListener('click', () => {
             playPause.setAttribute('aria-pressed', this.paused.toString());
@@ -419,6 +427,27 @@ class StoryViewElement extends HTMLElement {
                 forward.click();
             if (event.key === 'ArrowLeft')
                 back.click();
+        }
+    }
+    itemByHash() {
+        const hash = (location.hash || '').slice(1);
+        if (hash.length === 0)
+            return;
+        return this.items.find((item) => item.id === hash);
+    }
+    checkHashId() {
+        const item = this.itemByHash();
+        if (!item)
+            return;
+        const index = this.items.indexOf(item);
+        if (this.currentIndex === index)
+            return;
+        this.currentIndex = index - 1;
+        if (!this.dialog.open) {
+            this.button.click();
+        }
+        else {
+            this.goTo(1);
         }
     }
     async fetchData(url) {
@@ -444,6 +473,8 @@ class StoryViewElement extends HTMLElement {
         else {
             this.appendImages();
         }
+        this.checkHashId();
+        window.addEventListener('hashchange', this.checkHashId.bind(this));
     }
     pause() {
         this.paused = true;
@@ -487,7 +518,6 @@ class StoryViewElement extends HTMLElement {
             this.dialog.classList.remove('loading');
             this.bindEvents();
         }
-        this.currentIndex || (this.currentIndex = -1);
         this.goTo();
     }
     async goTo(delta = null) {
@@ -519,6 +549,7 @@ class StoryViewElement extends HTMLElement {
         const item = this.items[this.currentIndex];
         this.time.textContent = this.relativeTime(item.date_published);
         this.meta.textContent = item.summary;
+        this.link.href = `#${item.id}`;
         if (this.currentIndex > this.count - 1)
             this.currentIndex = 0;
         this.timer = setTimeout(this.goTo.bind(this), this.duration * 1000);
