77bfff87eaab2d0595fa23eac15771ff2bbe3846
Mu-An Chiou <github@muan.dev>
2024-11-03 15:55:48 +0800

    Use adoptedstylesheet to fix CSP

--- src/index.ts
+++ src/index.ts
@@ -1,421 +1,5 @@
 import {OpenStoriesFeed} from 'openstories-types'
 
-function css(duration: number) {
-  return `
-  :host {
-    display: inline-block;
-    font-family: system-ui, sans-serif;
-    --magic-h: 88vh;
-    --magic-w: 88vw;
-    --duration: ${duration}s;
-  }
-
-  ::backdrop {
-    background-color: rgba(0, 0, 0, 0.9);
-  }
-
-  dialog button {
-    border: 0;
-    background: 0;
-    appearance: none;
-    cursor: pointer;
-    padding: 0;
-    margin: 0;
-  }
-
-  dialog:focus:not(:focus-visible) {
-    outline: none;
-  }
-  
-  .side-controls {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    gap: 0.7vh;
-  }
-
-  #close,
-  #toggleMute,
-  #play,
-  #pause,
-  .mute,
-  .unmute {
-    display: none;
-  }
-
-  dialog:has(video.shown) #toggleMute {
-    display: block;
-  }
-  
-  dialog.is-muted .mute {
-    display: block;
-  }
-
-  dialog:not(.is-muted) .unmute {
-    display: block;
-  }
-
-  dialog.is-paused #play {
-    display: block;
-  }
-
-  dialog:not(.is-paused) #pause {
-    display: block;
-  }
-
-  :host(open-stories.is-empty) .ring {
-    opacity: 0.5;
-  }
-
-  :host(open-stories:not(.is-read):not(.is-empty)) .ring {
-    border: 2px solid #08c;
-    margin: 0;
-  }
-
-  .avatar {
-    width: 100%;
-    aspect-ratio: 1;
-    border-radius: 50%;
-  }
-
-  dialog {
-    height: min(var(--magic-h), var(--magic-w) * 16/9);
-    padding: 0;
-    border: 0;
-    aspect-ratio: 9/16;
-    background: transparent;
-    overflow: visible;
-    max-height: var(--magic-h);
-    max-width: var(--magic-w);
-  }
-  
-  #stories {
-    overflow: hidden;
-    height: 100%;
-    width: 100%;
-    position: absolute;
-    background: #000;
-    border-radius: 10px;
-  }
-
-  #stories .story {
-    position: absolute;
-    max-height: 100%;
-    max-width: 100%;
-    width: 100%;
-    aspect-ratio: 9/16;
-    object-fit: contain;
-    top: 0;
-    opacity: 0;
-    pointer-events: none;
-  }
-
-  #stories .story.shown {
-    pointer-events: auto;
-    opacity: 1;
-  }
-
-  .bar {
-    border-radius: 3px;
-    overflow: hidden;
-    height: 100%;
-    background: rgba(200, 200, 200, .2);
-    z-index: 1;
-    flex: auto;
-    box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.5);
-    transition: all 200ms;
-  }
-
-  #bars:hover .bar {
-    height: 4px;
-    transform: translateY(-1px);
-    background: rgba(200, 200, 200, .5);
-  }
-
-  .bar:hover {
-    transform: translateY(-1px);
-    flex-shrink: 0;
-    transform-origin: center;
-    min-width: 20px;
-    height: 4px;
-  }
-
-  #bars:hover {
-    gap: 2px;
-  }
-
-  #bars {
-    left: 0; 
-    right: 0;
-    top: 0;
-    height: 2px;
-    position: absolute;
-    padding: 10px;
-    display: flex;
-    gap: 3px;
-    z-index: 2;
-  }
-
-  .side-controls button,
-  .side-controls a {
-    display: inline-flex;
-  }
-
-  #time {
-    position: absolute;
-    margin: 10px;
-    top: 0; 
-    left: 0;
-    z-index: 1;
-  }
-
-  #time, {
-    color: rgba(255, 255, 255, 0.7);
-  }
-
-  #time,
-  #metadata,
-  #more {
-    color: #fff;
-    font-size: 1.7vh;
-    font-weight: 600;
-    text-shadow: 0 0 2px black;;
-  }
-
-  svg {
-    width: auto;
-    height: 3.5vh;
-    filter: drop-shadow(0 0 3px rgba(0, 0, 0, .5));
-    line-height: 0;
-  }
-
-  #bottom-controls {
-    position: absolute;
-    z-index: 1;
-    bottom: 0;
-    left: 0;
-    right: 0;
-    padding: 10px;
-    display: flex;
-    align-items: end;
-    overflow: hidden;
-    pointer-events: none;
-    gap: 1vh;
-  }
-
-  .action {
-    cursor: pointer;
-    transition: transform .3s;
-  }
-
-  .action:not([disabled]):hover,
-  .action:not([disabled]):focus {
-    transform: scale(1.2);
-  }
-
-  #open-heart {
-    left: auto;
-    right: 0;
-    display: inline-flex;
-  }
-
-  #open-heart .off {
-    transition: opacity .3s;
-  }
-
-  #open-heart .on {
-    position: absolute;
-    z-index: 1;
-    opacity: 0;
-    transform: scale(0);
-    transition: transform .3s;
-  }
-
-  #open-heart[aria-pressed="true"] .on { 
-    transform: scale(1);
-    opacity: 1;
-  }
-
-  #open-heart[aria-pressed="true"] .off { opacity: 0; }
-
-  #open-heart[aria-pressed="true"] path { fill: #f00; }
-
-  #open-heart[aria-busy="true"] { animation: pulsate .4s infinite; }
-
-  @keyframes pulsate {
-    0% { transform: scale(1) }
-    100% { transform: scale(1.2) }
-  }
-
-  #open-heart[errored] {
-    opacity: .5;
-  }
-
-  #metadata-details {
-    display: flex;
-    flex: 1 1 auto;
-    align-items: end;
-    overflow: hidden;
-    padding: 0.7vh 0;
-    gap: 0.7vh; 
-  }
-
-  #metadata,
-  #more {
-    line-height: 1.5em;
-  }
-
-  .is-collapsed #more {
-    display: block;
-  }
-  
-  #more {
-    display: none;
-    cursor: pointer;
-  }
-
-  #more,
-  .action {
-    pointer-events: auto;
-  }
-
-  #metadata {
-    flex: 1 1 auto;
-    white-space: nowrap;
-  }
-  
-  .is-collapsed #metadata {
-    overflow: hidden;
-    text-overflow: ellipsis;
-  }
-
-  .is-expanded #metadata {
-    white-space: normal;
-  }
-
-  #metadata a {
-    color: #000;
-  }
-
-  .progress {
-    height: 100%;
-    animation: none;
-    background-color: #fff;
-  }
-  
-  .progressing ~ .bar .progress {
-    background-color: transparent;
-    width: auto;
-  }
-
-  .is-loading .progressing .progress,
-  .is-paused .progressing .progress {
-    animation-play-state: paused;
-  }
-
-  .progressing .progress {
-    width: 0;
-    animation: progress var(--duration) linear;
-    animation-play-state: running;
-  }
-
-  @keyframes progress {
-    0% { width: 0%; }
-    100% { width: 100%; }
-  }
-
-  .is-loading button:not(.bar),
-  .is-loading #controls,
-  .is-loading #open-heart {
-    display: none;
-  }
-
-  .is-loading #stories img {
-    opacity: 0;
-  }
-
-  .is-loading .loading-visual {
-    display: block;
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    width: 2vh;
-    aspect-ratio: 1;
-    text-align: center;
-    background: #fff;
-    z-index: 1;
-    margin-left: -1vh;
-    animation: rotate 2s linear infinite;
-    font-size: 14px;
-  }
-
-  @keyframes rotate {
-    0% { transform: rotate(0deg); }
-    100% { transform: rotate(360deg); }
-  }
-
-  .loading-visual {
-    display: none;
-  }
-
-  #goToBlock {
-    left: 50%;
-    transform: translate(-50%, -50%);
-    padding: 0 2vw;
-    aspect-ratio: 9 / 16;
-    height: min(var(--magic-h), var(--magic-w) * 16/9);
-    position: absolute;
-    top: 50%;
-    z-index: 1;
-    pointer-events: none;
-    box-sizing: border-box;
-  }
-
-  #back, #forward {
-    pointer-events: all;
-    position: absolute;
-    z-index: 1;
-    min-width: 40px;
-    height: calc(100% - 100px);
-    bottom: 50px;
-    padding: 0;
-    font-size: 3vh;
-    width: 12vh;
-    font-family: system-ui, sans-serif;
-    color: #fff;
-  }
-
-  #back {
-    left: -1.5em;
-    text-align: left;
-  }
-
-  #forward {
-    right: -1.5em;
-    text-align: right;
-  }
-
-  @media (max-width: 420px), screen and (orientation: portrait) {
-    :host {
-      --magic-h: calc(var(--mobileVh) * 97);
-      --magic-w: 100vw;
-    }
-
-    ::backdrop {
-      background-color: #000;
-    }
-
-    .side-controls #close {
-      display: inline-flex;
-    }
-  }
-
-  [hidden] {
-    display: none !important;
-  }
-`
-}
-
 class OpenStoriesElement extends HTMLElement {
   root: ShadowRoot
   dialog: HTMLDialogElement
@@ -589,9 +173,9 @@ class OpenStoriesElement extends HTMLElement {
     const src = this.src
     if (src) this.fetchData(src)
 
-    const style = document.createElement('style')
-    style.innerText = css(this.duration)
-    this.root.append(style)
+    const stylesheet = new CSSStyleSheet()
+    stylesheet.replaceSync(css(this.duration))
+    this.root.adoptedStyleSheets = [stylesheet]
 
     this.style.setProperty('--mobileVh', `${window.innerHeight * 0.01}px`)
 
@@ -1047,4 +631,420 @@ declare global {
   interface HTMLElementTagNameMap {
     'open-stories': OpenStoriesElement
   }
+}
+
+function css(duration: number) {
+  return `
+  :host {
+    display: inline-block;
+    font-family: system-ui, sans-serif;
+    --magic-h: 88vh;
+    --magic-w: 88vw;
+    --duration: ${duration}s;
+  }
+
+  ::backdrop {
+    background-color: rgba(0, 0, 0, 0.9);
+  }
+
+  dialog button {
+    border: 0;
+    background: 0;
+    appearance: none;
+    cursor: pointer;
+    padding: 0;
+    margin: 0;
+  }
+
+  dialog:focus:not(:focus-visible) {
+    outline: none;
+  }
+  
+  .side-controls {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 0.7vh;
+  }
+
+  #close,
+  #toggleMute,
+  #play,
+  #pause,
+  .mute,
+  .unmute {
+    display: none;
+  }
+
+  dialog:has(video.shown) #toggleMute {
+    display: block;
+  }
+  
+  dialog.is-muted .mute {
+    display: block;
+  }
+
+  dialog:not(.is-muted) .unmute {
+    display: block;
+  }
+
+  dialog.is-paused #play {
+    display: block;
+  }
+
+  dialog:not(.is-paused) #pause {
+    display: block;
+  }
+
+  :host(open-stories.is-empty) .ring {
+    opacity: 0.5;
+  }
+
+  :host(open-stories:not(.is-read):not(.is-empty)) .ring {
+    border: 2px solid #08c;
+    margin: 0;
+  }
+
+  .avatar {
+    width: 100%;
+    aspect-ratio: 1;
+    border-radius: 50%;
+  }
+
+  dialog {
+    height: min(var(--magic-h), var(--magic-w) * 16/9);
+    padding: 0;
+    border: 0;
+    aspect-ratio: 9/16;
+    background: transparent;
+    overflow: visible;
+    max-height: var(--magic-h);
+    max-width: var(--magic-w);
+  }
+  
+  #stories {
+    overflow: hidden;
+    height: 100%;
+    width: 100%;
+    position: absolute;
+    background: #000;
+    border-radius: 10px;
+  }
+
+  #stories .story {
+    position: absolute;
+    max-height: 100%;
+    max-width: 100%;
+    width: 100%;
+    aspect-ratio: 9/16;
+    object-fit: contain;
+    top: 0;
+    opacity: 0;
+    pointer-events: none;
+  }
+
+  #stories .story.shown {
+    pointer-events: auto;
+    opacity: 1;
+  }
+
+  .bar {
+    border-radius: 3px;
+    overflow: hidden;
+    height: 100%;
+    background: rgba(200, 200, 200, .2);
+    z-index: 1;
+    flex: auto;
+    box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.5);
+    transition: all 200ms;
+  }
+
+  #bars:hover .bar {
+    height: 4px;
+    transform: translateY(-1px);
+    background: rgba(200, 200, 200, .5);
+  }
+
+  .bar:hover {
+    transform: translateY(-1px);
+    flex-shrink: 0;
+    transform-origin: center;
+    min-width: 20px;
+    height: 4px;
+  }
+
+  #bars:hover {
+    gap: 2px;
+  }
+
+  #bars {
+    left: 0; 
+    right: 0;
+    top: 0;
+    height: 2px;
+    position: absolute;
+    padding: 10px;
+    display: flex;
+    gap: 3px;
+    z-index: 2;
+  }
+
+  .side-controls button,
+  .side-controls a {
+    display: inline-flex;
+  }
+
+  #time {
+    position: absolute;
+    margin: 10px;
+    top: 0; 
+    left: 0;
+    z-index: 1;
+  }
+
+  #time, {
+    color: rgba(255, 255, 255, 0.7);
+  }
+
+  #time,
+  #metadata,
+  #more {
+    color: #fff;
+    font-size: 1.7vh;
+    font-weight: 600;
+    text-shadow: 0 0 2px black;;
+  }
+
+  svg {
+    width: auto;
+    height: 3.5vh;
+    filter: drop-shadow(0 0 3px rgba(0, 0, 0, .5));
+    line-height: 0;
+  }
+
+  #bottom-controls {
+    position: absolute;
+    z-index: 1;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    padding: 10px;
+    display: flex;
+    align-items: end;
+    overflow: hidden;
+    pointer-events: none;
+    gap: 1vh;
+  }
+
+  .action {
+    cursor: pointer;
+    transition: transform .3s;
+  }
+
+  .action:not([disabled]):hover,
+  .action:not([disabled]):focus {
+    transform: scale(1.2);
+  }
+
+  #open-heart {
+    left: auto;
+    right: 0;
+    display: inline-flex;
+  }
+
+  #open-heart .off {
+    transition: opacity .3s;
+  }
+
+  #open-heart .on {
+    position: absolute;
+    z-index: 1;
+    opacity: 0;
+    transform: scale(0);
+    transition: transform .3s;
+  }
+
+  #open-heart[aria-pressed="true"] .on { 
+    transform: scale(1);
+    opacity: 1;
+  }
+
+  #open-heart[aria-pressed="true"] .off { opacity: 0; }
+
+  #open-heart[aria-pressed="true"] path { fill: #f00; }
+
+  #open-heart[aria-busy="true"] { animation: pulsate .4s infinite; }
+
+  @keyframes pulsate {
+    0% { transform: scale(1) }
+    100% { transform: scale(1.2) }
+  }
+
+  #open-heart[errored] {
+    opacity: .5;
+  }
+
+  #metadata-details {
+    display: flex;
+    flex: 1 1 auto;
+    align-items: end;
+    overflow: hidden;
+    padding: 0.7vh 0;
+    gap: 0.7vh; 
+  }
+
+  #metadata,
+  #more {
+    line-height: 1.5em;
+  }
+
+  .is-collapsed #more {
+    display: block;
+  }
+  
+  #more {
+    display: none;
+    cursor: pointer;
+  }
+
+  #more,
+  .action {
+    pointer-events: auto;
+  }
+
+  #metadata {
+    flex: 1 1 auto;
+    white-space: nowrap;
+  }
+  
+  .is-collapsed #metadata {
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
+
+  .is-expanded #metadata {
+    white-space: normal;
+  }
+
+  #metadata a {
+    color: #000;
+  }
+
+  .progress {
+    height: 100%;
+    animation: none;
+    background-color: #fff;
+  }
+  
+  .progressing ~ .bar .progress {
+    background-color: transparent;
+    width: auto;
+  }
+
+  .is-loading .progressing .progress,
+  .is-paused .progressing .progress {
+    animation-play-state: paused;
+  }
+
+  .progressing .progress {
+    width: 0;
+    animation: progress var(--duration) linear;
+    animation-play-state: running;
+  }
+
+  @keyframes progress {
+    0% { width: 0%; }
+    100% { width: 100%; }
+  }
+
+  .is-loading button:not(.bar),
+  .is-loading #controls,
+  .is-loading #open-heart {
+    display: none;
+  }
+
+  .is-loading #stories img {
+    opacity: 0;
+  }
+
+  .is-loading .loading-visual {
+    display: block;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    width: 2vh;
+    aspect-ratio: 1;
+    text-align: center;
+    background: #fff;
+    z-index: 1;
+    margin-left: -1vh;
+    animation: rotate 2s linear infinite;
+    font-size: 14px;
+  }
+
+  @keyframes rotate {
+    0% { transform: rotate(0deg); }
+    100% { transform: rotate(360deg); }
+  }
+
+  .loading-visual {
+    display: none;
+  }
+
+  #goToBlock {
+    left: 50%;
+    transform: translate(-50%, -50%);
+    padding: 0 2vw;
+    aspect-ratio: 9 / 16;
+    height: min(var(--magic-h), var(--magic-w) * 16/9);
+    position: absolute;
+    top: 50%;
+    z-index: 1;
+    pointer-events: none;
+    box-sizing: border-box;
+  }
+
+  #back, #forward {
+    pointer-events: all;
+    position: absolute;
+    z-index: 1;
+    min-width: 40px;
+    height: calc(100% - 100px);
+    bottom: 50px;
+    padding: 0;
+    font-size: 3vh;
+    width: 12vh;
+    font-family: system-ui, sans-serif;
+    color: #fff;
+  }
+
+  #back {
+    left: -1.5em;
+    text-align: left;
+  }
+
+  #forward {
+    right: -1.5em;
+    text-align: right;
+  }
+
+  @media (max-width: 420px), screen and (orientation: portrait) {
+    :host {
+      --magic-h: calc(var(--mobileVh) * 97);
+      --magic-w: 100vw;
+    }
+
+    ::backdrop {
+      background-color: #000;
+    }
+
+    .side-controls #close {
+      display: inline-flex;
+    }
+  }
+
+  [hidden] {
+    display: none !important;
+  }
+`
 }
